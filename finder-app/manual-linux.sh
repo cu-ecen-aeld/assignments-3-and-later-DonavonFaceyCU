3ee00fc2cafcf0808c8b2867f67161309cea5b26
stealthadmin <stealthadmin@Ubuntu.myguest.virtualbox.org>
2025-09-14 22:33:18 -0600

    Script working, but full_test.sh hangs on the git clone

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -21,6 +21,7 @@ else
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
+rm -rf ${OUTDIR}
 mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
@@ -34,10 +35,18 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # Add your kernel build steps here
+    
+    make ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" mrproper
+    make ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" defconfig
+    make -j$(nproc) ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" all
+    make ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" modules
+    make ARCH="$ARCH" CROSS_COMPILE="$CROSS_COMPILE" dtbs
+
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,34 +56,82 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Create necessary base directories
 
 cd "$OUTDIR"
+mkdir -p rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+
+cd "$OUTDIR"
+rm -rf busybox
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
-else
+    # Configure busybox
+    make distclean
+    make defconfig
+
+    else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Make and install busybox
+
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Add library dependencies to rootfs
 
-# TODO: Make device nodes
+SYSROOT_DIR=$(${CROSS_COMPILE}gcc -print-sysroot)
 
-# TODO: Clean and build the writer utility
+cp $SYSROOT_DIR/lib/ld-linux-aarch64.so.1 lib/
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+cp $SYSROOT_DIR/lib64/libm.so.6 lib64/
+cp $SYSROOT_DIR/lib64/libc.so.6 lib64/
+cp $SYSROOT_DIR/lib64/libresolv.so.2 lib64/
+
+# Make device nodes
+
+sudo rm -f dev/null
+sudo rm -f dev/console
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
+
+# Clean and build the writer utility
+
+cd "$FINDER_APP_DIR"
+make clean
+make CROSS_COMPILE="$CROSS_COMPILE"
+
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
 
-# TODO: Chown the root directory
+cd ${OUTDIR}/rootfs
 
-# TODO: Create initramfs.cpio.gz
+mkdir -p home/conf
+
+cp "$FINDER_APP_DIR/writer" home/
+cp "$FINDER_APP_DIR/finder.sh" home/
+cp "$FINDER_APP_DIR/conf/username.txt" home/conf/
+cp "$FINDER_APP_DIR/conf/assignment.txt" home/conf/
+cp "$FINDER_APP_DIR/finder-test.sh" home/
+
+cp "$FINDER_APP_DIR/autorun-qemu.sh" home/
+
+# Chown the root directory
+
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+
+# Create initramfs.cpio.gz
+cd ..
+gzip -f initramfs.cpio
